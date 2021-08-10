847b09dde36167a4b22cef1fb488d73a4883230e
daxesh016 <daxesh016@gmail.com>
2021-08-10 06:48:24 -0700

    Add project files.

---
+++ Models/BookContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_BookAPI.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookItem> BookItems { get; set; }
+    }
+}
---
+++ Models/BookItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_BookAPI.Models
+{
+    public class BookItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Auther { get; set; }
+        public decimal Price { get; set; }
+    }
+}
